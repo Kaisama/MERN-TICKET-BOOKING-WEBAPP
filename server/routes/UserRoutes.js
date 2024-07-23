6f7de870104ee17a6d664f28964cb93519e78426
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-23 16:54:22 +0530

    update and delete route

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { signUp, getAllUsers } from '../controllers/UserController.js';
+import { signUp, getAllUsers, updateUser, deleteUser } from '../controllers/UserController.js';
 
 const UserRouter = express.Router();
 
-UserRouter.get("/allUser",getAllUsers)
-UserRouter.post("/signup",signUp)
+UserRouter.get("/",getAllUsers);
+UserRouter.post("/signup",signUp);
+UserRouter.patch("/updateUser/:id",updateUser);
+UserRouter.delete("/:id",deleteUser);
+
+
 
 
 export default UserRouter;
