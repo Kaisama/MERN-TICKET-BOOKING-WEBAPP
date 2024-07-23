5780cfa76b0d6e52a8986da9eeb963f64b17cbd5
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-23 12:30:24 +0530

    implement routes and controller

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import express from"express";
 import connectDB from "./database/db.js";
 import dotenv from "dotenv";
+import UserRouter from "./routes/UserRoutes.js";
 const app=express();
 
 dotenv.config();
 const port=process.env.PORT;
+app.use(express.json());
+
 connectDB();
 
+
+//middlewares
+app.use("/user",UserRouter)
 
 app.listen(port,()=>{
     console.log(`server is running at ${port}`)
