f7e10c447c0117e43317e5567694c647d29bb316
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-23 16:54:36 +0530

    update and delete controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,21 +1,11 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs';
 
-export const getAllUsers =async(req,res,next)=>{
-    let users;
-    try {
-        users = await User.find();
-        
-    } catch (error) {
-        return console.log(error);
-    }
-
-    if(!users){
-        return res.status(500).json({message:"Unexpected Error Occur"})
-    }
-
-    return res.status(200).json({users})
-}
+export const getAllUsers = async (req, res) => {
+    const users = await User.find();
+    res.json({users})
+  };
+  
 
 export const signUp=async(req,res)=>{
 
@@ -41,3 +31,43 @@ export const signUp=async(req,res)=>{
 
         return res.status(201).json({user})
 }
+
+
+export const updateUser=async(req,res)=>{
+ const id=req.params.id;
+ const {name,email,password}=req.body;
+ if(!name && name.trim()==="" && !email && email.trim()==="" && !password && password.trim()===""){
+    return res.status(422).json({message:"Invalid Input"})
+}
+
+let user;
+const hashPassword=bcrypt.hashSync(password);
+try {
+    user=await User.findByIdAndUpdate(id,{name,email,password:hashPassword});
+} catch (error) {
+    console.log(error)
+}
+if(!user){
+    return res.status(500).json({message:"Unexpected Error Occur"})
+}
+
+return res.status(200).json({message:"Updated Successfully"})
+
+}
+
+export const deleteUser=async(req,res)=>{
+    const id=req.params.id;
+    let user;
+    try {
+        user=await User.findByIdAndDelete(id);
+    
+    } catch (error) {
+        console.log(error);
+    }
+    if(!user){
+        return res.status(500).json({message:"Unexpected Error Occur"})
+    }
+    return res.status(200).json({message:"Deleted Successfully"})
+
+}
+
