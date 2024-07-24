a382fc002e62860cac967f979e2e4b9eb12e2c95
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-24 15:31:31 +0530

    add login route

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { signUp, getAllUsers, updateUser, deleteUser } from '../controllers/UserController.js';
+import { signUp, getAllUsers, updateUser, deleteUser ,loginUser} from '../controllers/UserController.js';
 
 const UserRouter = express.Router();
 
 UserRouter.get("/",getAllUsers);
 UserRouter.post("/signup",signUp);
-UserRouter.patch("/updateUser/:id",updateUser);
+UserRouter.patch("/:id",updateUser);
 UserRouter.delete("/:id",deleteUser);
+UserRouter.post("/login",loginUser);
 
 
 
