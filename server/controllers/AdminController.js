83283d758757cc7246eeb709382f0fb5430f1806
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-24 16:17:30 +0530

    use jwt token for admin login

---
+++ server/controllers/AdminController.js
@@ -0,0 +1,58 @@
+import Admin from "../models/Admin.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const adminSignup=async(req,res)=>{
+    const{email,password}=req.body;
+    let existingAdmin;
+    if(!email && email.trim()==="" && !password && password.trim()===""){
+        return res.status(422).json({message:"Invalid Inputs"})
+    }
+    try {
+        existingAdmin=await Admin.findOne({email})
+    } catch (error) {
+        return console.log(error);
+    }
+
+    if(existingAdmin){
+        return res.status(400).json({message:"Admin already exists"})
+    }
+
+    let admin;
+    const hashPassword=bcrypt.hashSync(password);
+    try{
+        admin =new Admin({email,password:hashPassword});
+        admin= await admin.save()
+    }catch(error){
+      return  console.log(error);
+    }
+    if(!admin){
+        return res.status(500).json({message:"Unable to store admin"})
+    }
+    return res.status(201).json({admin})
+}
+
+export const adminLogin=async(req,res)=>{
+    const {email,password}=req.body;
+    if(!email && email.trim()==="" && !password && password.trim()===""){
+        return res.status(422).json({message:"Invalid Inputs"})
+    }
+    let existingAdmin;
+    try {
+        existingAdmin=await Admin.findOne({email})
+    } catch (error) {
+        return console.log(error);
+    }
+    if(!existingAdmin){
+        return res.status(400).json({message:"Admin not found"})
+    }
+    const isPasswordCorrect = bcrypt.compareSync(password,existingAdmin.password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:"Invalid Credentials"})
+    }
+
+    const token= jwt.sign({id:existingAdmin._id},process.env.JWT_SECRET_KEY,{
+        expiresIn:"1d",
+    })
+
+    return res.status(200).json({message:"Authentication Completed",token,id:existingAdmin._id})
+}
