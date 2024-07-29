293d9933420f0582775d3c4fa8a729bf9fd2b9ea
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-29 17:33:55 +0530

    dynamic card item

--- client/src/components/Movies/MovieItem.jsx
+++ client/src/components/Movies/MovieItem.jsx
@@ -1,19 +1,18 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 
-const MovieItem = () => {
+const MovieItem = ({title,releaseDate,posterUrl,id}) => {
   return (
     <Card sx={{ margin:2,maxWidth: 250,height:320,borderRadius:5,":hover":{
         boxShadow:"10px 10px 20px #ccc"
     } }}>
-      <img height={"50%"} width={"100%"}/>
+      <img height={"50%"} width={"100%"} src={posterUrl} alt={title}/>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {new Date(releaseDate).toDateString()}
         </Typography>
       </CardContent>
       <CardActions>
