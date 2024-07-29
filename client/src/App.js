3f56c5bf6a769288dff54546c35b6af9c46897b2
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-29 16:53:59 +0530

    implement router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
+import {Route,Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Movies from "./components/Movies/Movies";
+import Homepage from "./components/Homepage";
+import Admin from "./components/Admin/Admin";
+import Auth from "./components/Auth/Auth";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <section>
+        
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/movies" element={<Movies/>}/>
+          <Route path="/admin" element={<Admin/>}/>
+          <Route path="/auth" element={<Auth/>}/>
+        </Routes>
+      </section>
+
     </div>
   );
 }
