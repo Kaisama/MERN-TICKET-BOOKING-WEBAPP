4e922d90092073d486f964fa598fc3e5b968d565
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-29 17:34:17 +0530

    cors added and apply

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ import UserRouter from "./routes/UserRoutes.js";
 import adminRouter from "./routes/AdminRoutes.js";
 import movieRouter from "./routes/MovieRoutes.js";
 import bookingRouter from "./routes/BookingRoutes.js";
+import cors from 'cors';
 const app=express();
 
 dotenv.config();
 const port=process.env.PORT;
 app.use(express.json())
-
+app.use(cors())
 connectDB();
 
 
