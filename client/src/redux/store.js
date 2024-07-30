5cf873ae3c8b17e8d2a7c92f3c4e29f74c875207
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-30 15:03:48 +0530

    store  for redux

---
+++ client/src/redux/store.js
@@ -0,0 +1,43 @@
+import {configureStore, createSlice} from '@reduxjs/toolkit';
+
+const userSlice=createSlice({
+    name:'user',
+    initialState:{isLoggedIn :false},
+    reducers:{
+        login(state){
+            state.isLoggedIn=true;
+        },
+        logout(state){
+            state.isLoggedIn=false;
+        },
+    },
+})
+
+export const userActions=userSlice.actions;
+
+
+
+const adminSlice=createSlice({
+    name:'auth',
+    initialState:{isLoggedIn:false},
+    reducers:{
+        login(state){
+            state.isLoggedIn=true;
+        },
+        logout(state){
+            state.isLoggedIn=false;
+        },
+    },
+})
+
+export const adminActions=adminSlice.actions;
+
+
+
+export const store=configureStore({
+    reducer:{
+        user:userSlice.reducer,
+        auth:adminSlice.reducer,
+    }
+})
+
