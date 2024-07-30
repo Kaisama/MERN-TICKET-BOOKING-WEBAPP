bfb3ea336aab5fccdf88a399c031493f973ba0fb
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-30 16:08:37 +0530

    authentication done with local storage

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import AuthForm from './AuthForm'
-import { sendUserAuthRequest } from '../../Api-Helpers/api-helpers'
-import { useDispatch } from 'react-redux'
-import { userActions } from '../../redux/store'
+import React from 'react';
+import AuthForm from './AuthForm';
+import { sendUserAuthRequest } from '../../Api-Helpers/api-helpers';
+import { useDispatch } from 'react-redux';
+import { userActions } from '../../redux/store';
 
 const Auth = () => {
-  const dispatch=useDispatch();
-  const getData=(data)=>{
-    console.log("users",data);
-    sendUserAuthRequest(data.input,data.signup).then((res)=>console.log(res))
-    .then(()=>dispatch(userActions.login()))
-    .catch(err=>console.log(err))
-  }
-  return (
-    <div><AuthForm onSubmit={getData} isAdmin={false} /></div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Auth
+  const onResReceived = (data) => {
+    console.log("Response data received:", data);
+    if (data && data.id) {
+      console.log("User ID found:", data.id);
+      dispatch(userActions.login());
+      localStorage.setItem("userId", data.id);
+    } else {
+      console.error("User ID not found in response data");
+    }
+  };
+
+  const getData = async (data) => {
+    try {
+      console.log("Sending user auth request with data:", data);
+      const response = await sendUserAuthRequest(data.input, data.signup);
+      console.log("Response from sendUserAuthRequest:", response);
+      onResReceived(response);
+    } catch (err) {
+      console.error("Authentication failed:", err);
+    }
+  };
+
+  return (
+    <div>
+      <AuthForm onSubmit={getData} isAdmin={false} />
+    </div>
+  );
+};
+
+export default Auth;
