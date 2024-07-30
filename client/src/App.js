1920fd88f2f6257a3ddcff752ebd26ae9f7d5454
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-30 15:02:06 +0530

    implement redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,15 @@ import Movies from "./components/Movies/Movies";
 import Homepage from "./components/Homepage";
 import Admin from "./components/Admin/Admin";
 import Auth from "./components/Auth/Auth";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAdminLoggedIn = useSelector((state)=>state.admin?.isLoggedIn)
+  const isUserLoggedIn = useSelector((state)=>state.user?.isLoggedIn)
+  console.log("admin",isAdminLoggedIn);
+  console.log("user",isUserLoggedIn);
+
+
   return (
     <div className="App">
       <Header/>
--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import axios from 'axios';
+import {Provider} from 'react-redux';
+import { store } from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 axios.defaults.baseURL="http://localhost:8080";
 root.render(
+  <Provider store={store}>
   <BrowserRouter>
-    <App />
+        <App />
   </BrowserRouter>
+  </Provider> 
 );
 
 
