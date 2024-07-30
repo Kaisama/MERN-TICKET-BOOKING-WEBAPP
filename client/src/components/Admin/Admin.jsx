78d77faa3178742bf93940f51c6ef6b2d55fdc16
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-30 15:03:26 +0530

    redux for login and logout for admin

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import AuthForm from '../Auth/AuthForm'
+import { sendAdminAuthRequest } from '../../Api-Helpers/api-helpers';
+import { useDispatch } from 'react-redux';
+import { adminActions } from '../../redux/store';
 
 const Admin = () => {
-  const getData=(data)=>{
-console.log(data);
-  }
+  const dispatch=useDispatch();
+  const getData = (data) => {
+    console.log("admins",data);
+    sendAdminAuthRequest(data.input)
+      .then((res) => console.log(res))
+      .then(()=>dispatch(adminActions.login()))
+      .catch((err) => console.log("Authentication failed:", err));
+  };
   return (
     <div>
       <AuthForm onSubmit={getData} isAdmin={true}/>
