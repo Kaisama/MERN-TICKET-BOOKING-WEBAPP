ec9693e008f602a6112ff3e01cafaea862f44d7a
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-30 12:42:58 +0530

    user auth done

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import AuthForm from './AuthForm'
+import { sendUserAuthRequest } from '../../Api-Helpers/api-helpers'
 
 const Auth = () => {
+  const getData=(data)=>{
+    console.log(data);
+    sendUserAuthRequest(data.input,data.signup).then((res)=>console.log(res)).catch(err=>console.log(err))
+  }
   return (
-    <div>Auth</div>
+    <div><AuthForm onSubmit={getData} isAdmin={false} /></div>
   )
 }
 
