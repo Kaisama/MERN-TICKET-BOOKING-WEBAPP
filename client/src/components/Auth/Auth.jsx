baaba286871d51927fe3edcf9447bf3e53486d70
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-30 15:03:38 +0530

    redux for login and logout for user

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import AuthForm from './AuthForm'
 import { sendUserAuthRequest } from '../../Api-Helpers/api-helpers'
+import { useDispatch } from 'react-redux'
+import { userActions } from '../../redux/store'
 
 const Auth = () => {
+  const dispatch=useDispatch();
   const getData=(data)=>{
-    console.log(data);
-    sendUserAuthRequest(data.input,data.signup).then((res)=>console.log(res)).catch(err=>console.log(err))
+    console.log("users",data);
+    sendUserAuthRequest(data.input,data.signup).then((res)=>console.log(res))
+    .then(()=>dispatch(userActions.login()))
+    .catch(err=>console.log(err))
   }
   return (
     <div><AuthForm onSubmit={getData} isAdmin={false} /></div>
