486a648fcfb8d1aa6428a2242dc8649943ceb0b3
Akshat Bisht <bishtakshat64@gmail.com>
2024-07-31 16:29:12 +0530

    optimization

--- server/controllers/MovieController.js
+++ server/controllers/MovieController.js
@@ -60,7 +60,7 @@ export const getAllMovies = async (req, res) => {
 
         return res.status(200).json({ movies });
     } catch (error) {
-        // Log the error for debugging
+        
         console.error('Error fetching movies:', error);
         return res.status(500).json({ message: "Internal Server Error", error: error.message });
     }
--- server/models/Movie.js
+++ server/models/Movie.js
@@ -10,7 +10,7 @@ const movieSchema = new mongoose.Schema({
         required: true
     }, 
     actors: {
-        type: String,
+        type: Array,
         required: true
     },
     releaseDate: {
